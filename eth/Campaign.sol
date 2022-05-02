6661b8bfee005cbcde105b8e94df3a1c1a48ecfd
VictorPorlan <vporlan@cifpfbmoll.eu>
2022-05-02 18:20:47 +0200

    feat: dev ether

--- eth/Campaign.sol
+++ eth/Campaign.sol
@@ -4,9 +4,10 @@ pragma solidity ^0.8.9;
 
 contract Campaign  {
     struct Donator {
+        bool exists;
         string name;
         uint date;
-        uint amount;
+        uint[] donations;
         string comment;
     }
 
@@ -14,6 +15,8 @@ contract Campaign  {
     uint public minimumContribution;
     string public title;
     string public description;
+    address[] public membersList;
+    mapping(address => Donator) members;
 
     constructor (uint minimum, string memory titleCont, string memory descriptionCont) {
         manager = msg.sender;
@@ -22,5 +25,19 @@ contract Campaign  {
         description = descriptionCont;
     }
 
+    function newDonatorContribution(string memory nameDonator, string memory commentDonator) public payable {
+        require(msg.value > minimumContribution);
+        require(!members[msg.sender].exists);
+        membersList.push(msg.sender);
+        Donator storage newDonator = members[msg.sender];
+        newDonator.name = nameDonator;
+        newDonator.date = block.timestamp;
+        newDonator.donations.push(msg.value);
+        newDonator.comment = commentDonator;
+        newDonator.exists = true;
+    }
     
+    function donatorData(address donatorAddress) public view returns(Donator memory){
+        return members[donatorAddress];
+    }
 }
